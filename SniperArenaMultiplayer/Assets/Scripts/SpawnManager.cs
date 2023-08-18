cebfa6d2657b1b556d69f0ccb3c85bc6984660b6
Huszár András <hemcraft@gmail.com>
2023-08-18 14:12:11 +0200

    Position based spawning

--- SniperArenaMultiplayer/Assets/Scripts/SpawnManager.cs
+++ SniperArenaMultiplayer/Assets/Scripts/SpawnManager.cs
@@ -1,20 +1,89 @@
+using Fusion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public IdealSpawnSide idealSpawnSide;
+
     public List<Transform> allSpawnPoints;
+    public List<Transform> leftSpawnPoints;
+    public List<Transform> rightSpawnPoints;
+
+    private NetworkRunnerHandler networkRunnerHandler;
 
     private void Start()
     {
-        
+        networkRunnerHandler = FindObjectOfType<NetworkRunnerHandler>();
+
+        StartCoroutine(CheckForClosePlayersEverySecond());
     }
 
     public Transform GetSpawnPoint()
     {
-        int index = Random.Range(0, allSpawnPoints.Count);
+        CheckForClosePlayers();
 
-        return allSpawnPoints[index];
+        if (idealSpawnSide == IdealSpawnSide.Left)
+        {
+            int index = Random.Range(0, leftSpawnPoints.Count);
+
+            return leftSpawnPoints[index];
+
+        }
+        else if (idealSpawnSide == IdealSpawnSide.Right)
+        {
+            int index = Random.Range(0, rightSpawnPoints.Count);
+
+            return rightSpawnPoints[index];
+        }
+        else
+        {
+            int index = Random.Range(0, allSpawnPoints.Count);
+
+            return allSpawnPoints[index];
+        }
+    }
+
+    public enum IdealSpawnSide
+    {
+        All,
+        Left,
+        Right
+    }
+
+    private void CheckForClosePlayers()
+    {
+        idealSpawnSide = IdealSpawnSide.All;
+
+        foreach (PlayerRef player in networkRunnerHandler.networkRunner.ActivePlayers)
+        {
+            if (player != networkRunnerHandler.networkRunner.LocalPlayer)
+            {
+                NetworkObject playerObject = networkRunnerHandler.networkRunner.GetPlayerObject(player);
+
+                if (playerObject != null)
+                {
+                    //Debug.Log(playerObject.transform.position);
+                    if (playerObject.transform.position.z >= 0)
+                    {
+                        idealSpawnSide = IdealSpawnSide.Right;
+                    }
+                    else
+                    {
+                        idealSpawnSide = IdealSpawnSide.Left;
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator CheckForClosePlayersEverySecond()
+    {
+        yield return new WaitForSeconds(1f);
+
+        CheckForClosePlayers();
+
+        StartCoroutine(CheckForClosePlayersEverySecond());
     }
 }
